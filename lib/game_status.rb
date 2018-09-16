392f860b9a5b86017ea4205b17abfe12c4196fbe
Nicholas Betito <nbetito@gmail.com>
2018-09-16 04:21:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,29 +16,26 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-
-if draw?(board)
-  false
-else
-
-  WIN_COMBINATIONS.select do |win_combination|
-
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-    return win_combination
-
-    end
-
+# if draw?(board)
+#   false
+# else
+#   WIN_COMBINATIONS.select do |win_combination|
+#   win_index_1 = win_combination[0]
+#   win_index_2 = win_combination[1]
+#   win_index_3 = win_combination[2]
+#   position_1 = board[win_index_1]
+#   position_2 = board[win_index_2]
+#   position_3 = board[win_index_3]
+#     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+#     return win_combination
+#     end
+#   end
+# end
+  WIN_COMBINATIONS.detect do |win_combination| 
+    board[win_combination[0]] == board[win_combination[1]] &&
+    board[win_combination[1]] == board[win_combination[2]] &&
+    position_taken?(board, win_combination[0])
   end
-end
-
 end
 
 
